e63e364a7f12ce9d2d68ea3437732e7ac42f1e78
thanhtran0312 <151376396+thanhtran0312@users.noreply.github.com>
2026-07-12 11:22:12 +0200

    Update bids.sh

--- preprocessing/bids.sh
+++ preprocessing/bids.sh
@@ -2,7 +2,7 @@
 files=(*fif)
 i=1
 for f in $files; do
-  mv "$f" "sub-01_ses-01_task-IceSkating_run-$(printf "%02d" $i).fif"
-  i=$((i+1))
+  mv "$f" "sub-01_ses-01_task-IceSkating_run-$(printf "%02d" $i)_meg.fif"
+  i=$((i+1))  # (( )) for arithmetic expansion which can read i without $ vs. () for running command and taking output
 done
 
