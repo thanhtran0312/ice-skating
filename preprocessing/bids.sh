35d0e851df4dfa2ba152a8f75e45603ba85c160b
thanhtran0312 <151376396+thanhtran0312@users.noreply.github.com>
2026-08-06 01:15:36 +0200

    Delete preprocessing directory

--- preprocessing/bids.sh
+++
@@ -1,8 +0,0 @@
-# trying to change names to bid formats for one subject
-files=(*fif)
-i=1
-for f in $files; do
-  mv "$f" "sub-01_ses-01_task-IceSkating_run-$(printf "%02d" $i)_meg.fif"
-  i=$((i+1))  # (( )) for arithmetic expansion which can read i without $ vs. () for running command and taking output
-done
-
